d6c226668d1a5225ad7627997e8ac6f2a8791f07
Ernest Yusupov <ernest.math.yu@gmail.com>
2024-04-03 13:22:55 +0500

    Added file handling functions

--- python_basics/main.py
+++ python_basics/main.py
@@ -79,6 +79,100 @@ def list_manipulations() -> None:
     print("\nAfter clear")
     print("\nCopy of original list", some_copy_list)
     print("= list:", some_second_list)
+   
+def dict_and_set_manipulations() -> None:
+    some_dict: dict = {
+        "name": "John",
+        "age": 23,
+        "id": 100052
+    }
     
+    print("\nDict:", some_dict)
+    print("Get name from dict:", some_dict["name"])
+    
+    some_set: set = {
+        "apple",
+        "banana",
+        "cherry",
+        0, False,   # 0===False
+        1, True     # 1===True
+    }
+    print(some_set)
+   
+def file_manipulations(in_filename: str) -> None:
+    '''
+    f.open(filename, mode) <- base
+    
+    file = open(in_filename, "r")
+    print(file.read())  #string that contains all characters in file
+    
+    for each in file:
+        print(each, end="")
+    ''' 
+    with open(in_filename) as new_file:
+        data = new_file.readlines()
+        for line in data:
+            word = line.split()
+            print(word)
+            
+    with open(in_filename, "w") as write_file:
+        write_file.write("some new data")
+        write_file.write("My name is not Er")
+        
+    
+    with open(in_filename, "a") as append_file:
+        append_file.write("1234")
+        append_file.write("Abricos")
+        
+import os
+
+def create_file(filename: str) -> None:
+    try:
+        with open(filename, 'w') as f:
+            f.write('Hello world!\n')
+        print("File " + filename + " created successfully!")
+    except IOError:
+        print("Error: could not create file " + filename)
+
+def read_file(filename) -> None:
+    try:
+        with open(filename, 'r') as f:
+            content: str = f.read()
+            print(content)
+    except IOError:
+        print("Error: could not read file " + filename)
+        
+def append_file(filename, text) -> None:
+    try:
+        with open(filename, 'a') as f:
+            f.write(text)
+        print("Text appended to file " + filename + " success")
+    except IOError:
+        print("Error: could not append to file " + filename)
+        
+def rename_file(filename, new_filename) -> None:
+    try:
+        os.rename(filename, new_filename)
+        print("File " + filename + " renamed to " + new_filename
+              + "successfully")
+    except IOError:
+        print("Error: could not rename file " + filename)
+        
+def delete_file(filename) -> None:
+    try:
+        os.remove(filename)
+        print("File " + filename + " deleted successfully")
+    except IOError:
+        print("Error could not delete file " + filename)
+
 if __name__ == "__main__":
-    list_manipulations()
+    filename = "example.txt"
+    new_filename = "new_example.txt"
+ 
+    create_file(filename)
+    read_file(filename)
+    append_file(filename, "This is some additional text.\n")
+    read_file(filename)
+    rename_file(filename, new_filename)
+    read_file(new_filename)
+    delete_file(new_filename)
